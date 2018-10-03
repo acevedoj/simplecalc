a4f0b051d5b8377bf3febaa83b68223e8b2eb4f1
Juan Acevedo <acevedoj@D-10-19-159-120.dhcp4.washington.edu>
2018-10-03 11:34:52 -0700

    added calculator functions

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,6 +10,49 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        if (args[args.count - 1] == "count") {
+            return args.count - 1;
+        } else if (args[args.count - 1] == "fact") {
+            if (Int(args[args.count - 1]) == 0) {
+                return 1;
+            } else if ((args.count == 1 && args[0] == "fact")) {
+                return 0;
+            } else {
+                var number: UInt = UInt(args[0])!;
+                var factorial: UInt = 1;
+                for _ in (1...number).reversed() {
+                    factorial *= number;
+                    number = number - 1;
+                }
+                return Int(factorial);
+            }
+
+        } else if (args[args.count - 1] == "avg") {
+            if (args.count == 1) {
+                return 0;
+            }
+            var sum: Int = 0;
+            for index in 0...args.count - 2 {
+                sum += Int(args[index])!;
+            }
+            return sum / (args.count - 1);
+        } else {
+            let num1: Int! = Int(args[0]);
+            let num2: Int! = Int(args[2]);
+            if (args[1] == "+") {
+                return num1 + num2
+            } else if (args[1] == "-") {
+                return num1 - num2
+            } else if (args[1] == "/") {
+                return num1 / num2
+            } else if (args[1] == "*") {
+                return num1 * num2
+            } else if (args[1] == "%") {
+                return num1 % num2
+            } else {
+                return 10;
+            }
+        }
     }
     
     public func calculate(_ arg: String) -> Int {
@@ -23,4 +66,3 @@ let first = readLine()!
 let operation = readLine()!
 let second = readLine()!
 print(Calculator().calculate([first, operation, second]))
-
